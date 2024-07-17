02cd5578b84f051e924a9622211d5efb36541279
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-07-17 12:53:55 +1000

    frontend pages design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/auth/login/LoginPage';
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/login' element={<LoginPage />}></Route>
         <Route path='/signUp' element={<SignUpPage />}></Route>
+        <Route path='/notification' element={<NotificationPage />}></Route>
+        <Route path='/profile/:username' element={<ProfilePage />}></Route>
       </Routes>
       <RightPanel />
     </div>
