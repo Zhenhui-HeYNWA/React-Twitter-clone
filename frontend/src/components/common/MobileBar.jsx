4d221c2c30ac52979ff646701bf3dc378363efe6
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-07-21 01:47:01 +1000

    add logout button for mobile bar

--- frontend/src/components/common/MobileBar.jsx
+++ frontend/src/components/common/MobileBar.jsx
@@ -3,8 +3,36 @@ import { MdHomeFilled } from 'react-icons/md';
 import { IoNotifications } from 'react-icons/io5';
 import { MdPerson } from 'react-icons/md';
 import ThemeToggler from '../context/ThemeToggler';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import { BiLogOut } from 'react-icons/bi';
 
-const MobileBar = ({ authUser }) => {
+const MobileBar = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: logout } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch('/api/auth/logout', {
+          method: 'POST',
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(data.error || 'Something went wrong');
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['authUser'] });
+    },
+    onError: () => {
+      toast.error('Logout failed');
+    },
+  });
+
+  const { data: authUser } = useQuery({ queryKey: ['authUser'] });
   return (
     <div className='md:hidden fixed bottom-0 left-0 w-full bg-gray-100 dark:bg-secondary border-t border-gray-200 dark:border-gray-700'>
       <div className='flex justify-around items-center p-2'>
@@ -24,6 +52,17 @@ const MobileBar = ({ authUser }) => {
           <MdPerson className='w-6 h-6' />
         </Link>
         <ThemeToggler />
+        {authUser && (
+          <>
+            <BiLogOut
+              className='w-6 h-6 cursor-pointer text-gray-800 dark:text-white'
+              onClick={(e) => {
+                e.preventDefault();
+                logout();
+              }}
+            />
+          </>
+        )}
       </div>
     </div>
   );
