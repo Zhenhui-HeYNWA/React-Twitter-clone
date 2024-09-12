d1fe1c1e3160e2d167366e2702003f35799e8769
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-09-12 10:53:58 +1000

    test

--- frontend/src/components/common/MentionComponent.jsx
+++ frontend/src/components/common/MentionComponent.jsx
@@ -3,20 +3,20 @@ import { Mention } from 'primereact/mention';
 
 const MentionComponent = (props) => {
   const { value, ...rest } = props; // Exclude autoResize from DOM elements
-  const mentionRef = useRef(null); // Ref for accessing Mention input
+  // const mentionRef = useRef(null); // Ref for accessing Mention input
 
-  const textarea = document.querySelector('textarea');
+  // const textarea = document.querySelector('textarea');
 
-  textarea?.addEventListener('input', autoResize);
+  // textarea?.addEventListener('input', autoResize);
 
-  function autoResize() {
-    this.style.height = 'auto'; // Reset height
-    this.style.height = this.scrollHeight + 'px'; // Set height based on content
-  }
+  // function autoResize() {
+  //   this.style.height = 'auto'; // Reset height
+  //   this.style.height = this.scrollHeight + 'px'; // Set height based on content
+  // }
 
   return (
     <Mention
-      ref={mentionRef} // Attach ref to Mention
+      // ref={mentionRef} // Attach ref to Mention
       value={value} // Pass the value
       {...rest} // Pass the remaining props
     />
